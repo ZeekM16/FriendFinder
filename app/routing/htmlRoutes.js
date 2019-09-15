ba146142156f42d436a996b9243a230efb5171f1
Isaac Molina <isaacmolina.m16@gmail.com>
2019-09-15 15:41:58 -0500

    updated routes and added html to survey and home

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,10 @@
+var require =('path');
+
+module.exports = function(app){
+    app.get('/survey', function(req, res){
+        res.sendFile(path.join(__dirname + '/../public/survey.html'));
+    })
+    app.use('/survey', function(req, res){
+        res.sendFile(path.join(__dirname + '/../public/home.html'));
+    })
+}
